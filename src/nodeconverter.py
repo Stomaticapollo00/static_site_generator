f4ff7c88c6fec97534f4ba8d21302cac7216060b
Stomaticapollo00 <gmschaffran@hotmail.com>
2026-04-07 08:23:11 -0700

    1.0.4 Improved nodeconversion and added nodesplitter function

--- src/nodeconverter.py
+++ src/nodeconverter.py
@@ -30,3 +30,29 @@ def text_node_to_html_node(text_node):
                 )
         case _:
             raise Exception("Text type is not supported.")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+        else:
+            old_text = old_node.text
+            if old_text.count(delimiter) >= 2 and old_text.count(delimiter) % 2 == 0:
+                while delimiter in old_text:
+                    tmp_text_list = old_text.split(delimiter, 2)
+                    for i in range(0, len(tmp_text_list)):
+                        if i == 0 and tmp_text_list[i] != "":
+                            tmp_node = TextNode(tmp_text_list[i], TextType.TEXT)
+                            new_nodes.append(tmp_node)
+                        elif i == 1:
+                            tmp_node = TextNode(tmp_text_list[i], text_type)
+                            new_nodes.append(tmp_node)
+                    old_text = tmp_text_list[-1]
+                if old_text != "":
+                    new_nodes.append(TextNode(old_text, TextType.TEXT))
+            elif delimiter in old_text:
+                raise Exception("Delimiter found in text but not closed properly")
+            else:
+                new_nodes.append(old_node)
+    return new_nodes
--- src/test_nodeconverter.py
+++ src/test_nodeconverter.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from nodeconverter import text_node_to_html_node
+from nodeconverter import text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextToHTMLConverter(unittest.TestCase):
@@ -48,3 +48,75 @@ class TestTextToHTMLConverter(unittest.TestCase):
         node = TextNode("This is a hyperlink text node", "hyperlink", "https://www.google.com")
         with self.assertRaises(Exception):
             text_node_to_html_node(node)
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_splitting(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        node2 = TextNode("This is text with a **bolded phrase** in the middle", TextType.TEXT)
+        node3 = TextNode("This is text with a bold phrase at **the end**", TextType.TEXT)
+        node4 = TextNode("This is **text** with a few **bold** words **in it**", TextType.TEXT)
+        node5 = TextNode("**This is text** with a bold phrase at the beginning", TextType.TEXT)
+        node6 = TextNode("This is `text without closing the delimiter", TextType.TEXT)
+        node7 = TextNode("This is _text_ with **multiple** delimiters", TextType.TEXT)
+        node8 = TextNode("This is already bold", TextType.BOLD)
+        node9 = TextNode("This is already italic", TextType.ITALIC)
+        node10 = TextNode("This is just text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        new_nodes2 = split_nodes_delimiter([node2, node3, node4], "**", TextType.BOLD)
+        new_nodes3 = split_nodes_delimiter([node5], "**", TextType.BOLD)
+        new_nodes4 = split_nodes_delimiter(split_nodes_delimiter([node7], "_", TextType.ITALIC), "**", TextType.BOLD)
+        new_nodes5 = split_nodes_delimiter([node2, node8, node9, node10], "**", TextType.BOLD)
+        self.assertEqual(
+                            new_nodes,
+                            [
+                                TextNode("This is text with a ", TextType.TEXT),
+                                TextNode("code block", TextType.CODE),
+                                TextNode(" word", TextType.TEXT),
+                            ],
+                        )
+        self.assertEqual(
+                            new_nodes2,
+                            [
+                                TextNode("This is text with a ", TextType.TEXT),
+                                TextNode("bolded phrase", TextType.BOLD),
+                                TextNode(" in the middle", TextType.TEXT),
+                                TextNode("This is text with a bold phrase at ", TextType.TEXT),
+                                TextNode("the end", TextType.BOLD),
+                                TextNode("This is ", TextType.TEXT),
+                                TextNode("text", TextType.BOLD),
+                                TextNode(" with a few ", TextType.TEXT),
+                                TextNode("bold", TextType.BOLD),
+                                TextNode(" words ", TextType.TEXT),
+                                TextNode("in it", TextType.BOLD),
+                            ],
+                        )
+        self.assertEqual(
+                            new_nodes3,
+                            [
+                                TextNode("This is text", TextType.BOLD),
+                                TextNode(" with a bold phrase at the beginning", TextType.TEXT),
+                            ],
+                        )
+        self.assertEqual(
+                            new_nodes4,
+                            [
+                                TextNode("This is ", TextType.TEXT),
+                                TextNode("text", TextType.ITALIC),
+                                TextNode(" with ", TextType.TEXT),
+                                TextNode("multiple", TextType.BOLD),
+                                TextNode(" delimiters", TextType.TEXT),
+                            ],
+                        )
+        self.assertEqual(
+                            new_nodes5,
+                            [
+                                TextNode("This is text with a ", TextType.TEXT),
+                                TextNode("bolded phrase", TextType.BOLD),
+                                TextNode(" in the middle", TextType.TEXT),
+                                TextNode("This is already bold", TextType.BOLD),
+                                TextNode("This is already italic", TextType.ITALIC),
+                                TextNode("This is just text", TextType.TEXT),
+                            ],
+                        )
+        with self.assertRaises(Exception):
+            split_nodes_delimiter([node6], "`", TextType.CODE)
